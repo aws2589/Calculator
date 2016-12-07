843d92df14c1e98fa458f478a64ac6523805ba39
Abdul Shahid <aws2589@email.vccs.edu>
2016-12-07 11:26:09 -0500

    calculator with array program

---
+++ CalcDriver.java
@@ -0,0 +1,13 @@
+
+public class CalcDriver {
+
+	public static void main(String [] args) {
+		
+		
+		System.out.println(Calculator.getSum());
+		System.out.println(Calculator.getAverage());
+		System.out.println(Calculator.getProduct());
+		System.out.println(Calculator.getFactorial());
+	}
+}
+
---
+++ Calculator.java
@@ -0,0 +1,31 @@
+
+public class Calculator {
+	private double [] array = new double [] {1,2,3,4};
+	
+	public static double getSum(){
+		return 1+2+3+4;
+		
+	}
+	public static double getAverage(){
+		return ((1+2+3+4)/4);
+		
+	}
+	public static double getProduct(){
+		return 1*2*3*4;
+		
+	}
+	public static double getFactorial(){
+		return 4*3*2*1;
+		
+	}
+	public double [] getArray(){
+		return array;
+		
+	}
+	public void setArray(double [] array){
+		this.array = array;
+		
+	}
+	}
+
+
